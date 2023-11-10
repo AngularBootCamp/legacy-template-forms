c1fac5f57492c98034954eab0e5ce2ddb3e30b52
Lance Finney <lance.finney@oasisdigital.com>
2023-11-10 11:15:50 -0600

    curriculum update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,13 @@
+import { JsonPipe } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  standalone: true,
+  imports: [FormsModule, JsonPipe]
 })
 export class AppComponent {
   credentials: { login: string; password: string };
