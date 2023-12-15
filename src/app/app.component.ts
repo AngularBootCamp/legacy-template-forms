0616d7c7e08c2e18cdb868f106dc02134d370ec6
Lance Finney <lance.finney@oasisdigital.com>
2023-12-15 16:41:27 -0600

    curriculum update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,7 @@ import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+  styleUrl: './app.component.scss',
   standalone: true,
   imports: [FormsModule, JsonPipe]
 })
